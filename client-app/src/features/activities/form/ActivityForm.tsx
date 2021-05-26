12eb74c527406eb544e711307e14ff447c88d3a4
jeastman85 <jeff.eastman@outlook.com>
2021-05-26 07:15:23 -0400

    End of section 15

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router'
 import { Button, Header, Segment } from 'semantic-ui-react'
 import LoadingComponent from '../../../app/layout/LoadingComponent'
-import { Activity } from '../../../app/models/activity'
+import { ActivityFormValues } from '../../../app/models/activity'
 import { useStore } from '../../../app/stores/store'
 import { v4 as uuid } from 'uuid';
 import { Link } from 'react-router-dom'
@@ -23,15 +23,7 @@ export default observer(function ActivityForm(){
     const {selectedActivity, createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams<{id: string}>();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -44,13 +36,13 @@ export default observer(function ActivityForm(){
     })
 
 useEffect(() => {
-    if (id) loadActivity(id).then(activity => setActivity(activity!))
+    if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
 }, [id, loadActivity]);
 
 
 
-    function handleFormSubmit(activity: Activity){
-        if (activity.id.length === 0){
+    function handleFormSubmit(activity: ActivityFormValues){
+        if (!activity.id){
             let newActivity = {
                 ...activity,
                 id:uuid()
@@ -89,7 +81,7 @@ if(loadingInitial) return <LoadingComponent content='Loading activity...' />
                         <MyTextInput placeholder="Venue" name='venue'  />
                         <Button 
                         disabled={isSubmitting || !dirty || !isValid }
-                        loading={loading} 
+                        loading={isSubmitting} 
                         floated='right' 
                         positive type='submit' 
                         content='Submit' />
