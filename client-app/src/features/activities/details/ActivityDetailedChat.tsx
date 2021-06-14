f6097fb2f7327e253dc6eafa3083cade1613ed4a
jeastman85 <jeff.eastman@outlook.com>
2021-06-14 08:12:09 -0400

    End of section 20

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -2,9 +2,8 @@ import { Field, FieldProps, Form, Formik } from 'formik';
 import { observer } from 'mobx-react-lite'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
-import { Segment, Header, Comment, Button, Loader } from 'semantic-ui-react'
+import { Segment, Header, Comment, Loader } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
-import MyTextArea from '../form/MyTextArea';
 import * as Yup from 'yup'
 import { formatDistanceToNow } from 'date-fns';
 
